2a994f956892b8336d8a61f4df94808c1811271d
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2024-03-25 08:35:11 -0400

    took out teams

--- sports-scheduler/models/Team.js
+++ sports-scheduler/models/Team.js
@@ -1,10 +1,10 @@
-//models/Team.js 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// //models/Team.js 
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const teamSchema = new Schema({
-  school: { type: String, required: true },
-  league: { type: Schema.Types.ObjectId, ref: 'League'} //might not need
-});
+// const teamSchema = new Schema({
+//   school: { type: String, required: true },
+//   league: { type: Schema.Types.ObjectId, ref: 'League'} //might not need
+// });
 
-module.exports = mongoose.model('Team', teamSchema);
+// module.exports = mongoose.model('Team', teamSchema);
