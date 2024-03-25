f1787998d76103a01cce097221888e0c35d9e7d2
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2024-03-25 09:50:36 -0400

    vue project

---
+++ sports-scheduler/client/src/views/EventDetailsView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="">
+      <h1>Event Details View</h1>
+    </div>
+</template>
+  
+  <style>
+  </style>
+  
---
+++ sports-scheduler/client/src/views/EventListView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+<template>
+  <h2>Event List</h2>
+</template>
+ 
